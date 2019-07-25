5de8a353a83dc73dc9e25f0a4a821a8ddbb4a8eb
Jakub Szturomski <x@2.pl>
2019-07-25 11:53:27 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/*
+ *  Created: 7/25/2019 11:27 AM
+ *  Author: jaredmeister
+ */
+public class Main
+{
+    public static void main(String a[])
+    {
+        long[] outOfMemory = new long[Integer.MAX_VALUE];
+    }
+}
