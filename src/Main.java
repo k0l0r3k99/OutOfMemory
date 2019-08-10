38d066fbfaf489c4de82c293342e50d607905f73
k0l0r3k99 <30705417+k0l0r3k99@users.noreply.github.com>
2019-08-10 02:48:10 +0200

    Fix args
    Thanks, vocan

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		args = new String[] { "direct" };
 		if (args.length == 0) {
 			System.out.println("Usage: [direct / heap]");
 			System.exit(1);
